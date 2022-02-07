417459e36916ed6beca3ecee27112963470e68fc
Manabu Ishii <manabu.ishii.rb@gmail.com>
2022-02-07 23:36:03 +0900

    Fix rubocop style

--- sparqlga.rb
+++ sparqlga.rb
@@ -15,56 +15,56 @@ class SPARQLGA < GeneticAlgorithm
     chromosome.new(value)
   end
 
-  def get_chr_size
+  def chr_size
     @@chr_size
   end
 
   def select(population)
     # sort by fitness_value
-    newary = population.sort! { |a,b| b.fitness_value <=> a.fitness_value }
+    newary = population.sort! { |a, b| b.fitness_value <=> a.fitness_value }
     # sum of fitness_value for selection
     sum = newary.inject(0) { |sum, ch| sum + ch.fitness_value }
     # select parent 1
     p1rand = rand(0..sum)
     f = 0
     p1 = newary[0]
-    newary.each{|ch|
+    newary.each do |ch|
       f += ch.fitness_value
       if f >= p1rand
         p1 = ch
         break
       end
-    }
+    end
     # select parent 2
     # parent 2 is not the same as parent 1
     sum -= p1.fitness_value
     p2rand = rand(0..sum)
     f = 0
     p2 = newary[1]
-    newary.each{|ch|
-      if ch == p1
-        next
-      end
+    newary.each do |ch|
+      next if ch == p1
+
       f += ch.fitness_value
       if f >= p2rand
         p2 = ch
         break
       end
-    }
+    end
     [p1, p2]
   end
 
-  def run(chromosome, p_cross, p_mutation, iterations = 100,population_size = 100)
+  def run(chromosome, p_cross, p_mutation, iterations = 100, population_size = 100)
     # initial population
     population = population_size.times.map { generate(chromosome) }
     current_generation = population
     next_generation    = []
     alltime_best = population[0]
 
-    iterations.times {|cnt|
+    iterations.times do |cnt|
       puts "Generation #{cnt}"
       # Exec fitness function
       current_generation.each { |ch| ch.fitness }
+
       # max
       best_fit = current_generation.max_by { |ch| ch.fitness_value }.dup
 
@@ -72,45 +72,45 @@ class SPARQLGA < GeneticAlgorithm
         alltime_best = best_fit.dup
       end
 
-      puts "Best fit: #{best_fit.value} => #{best_fit.fitness_value}, elapsed time: #{best_fit.get_elapsed_time}"
-      (population.size / 2).times {
+      puts "Best fit: #{best_fit.value} => #{best_fit.fitness_value}, elapsed time: #{best_fit.elapsed_time}"
+      (population.size / 2).times do
         selection = select(current_generation)
         # crossover
         selection = crossover(selection, chromosome)
         # mutation
         selection[0].mutate(p_mutation)
         selection[1].mutate(p_mutation)
-        # 
+        # set next generation
         next_generation << selection[0] << selection[1]
-      }
+      end
       # NOTE: last generation is not evaluated
       current_generation = next_generation
       next_generation    = []
-      timestr = SparqlChromosome.get_timestr
+      timestr = SparqlChromosome.timestr
       # save best fit
-      File.open("result/#{timestr}/#{cnt}_bestfit.txt", 'w') { |f| f.write("All times Best fit: Chr #{alltime_best.value} => #{alltime_best.fitness_value}, elapsed time: #{alltime_best.get_elapsed_time}") }
+      File.open("result/#{timestr}/#{cnt}_bestfit.txt", 'w') { |f| f.write("All times Best fit: Chr #{alltime_best.value} => #{alltime_best.fitness_value}, elapsed time: #{alltime_best.elapsed_time}") }
       # save fastest fit
-      File.open("result/#{timestr}/#{cnt}_fastest.txt", 'w') { |f| f.write("All times Fastest fit: Chr #{SparqlChromosome.get_fastest_chromosome} => #{SparqlChromosome.get_fastest_fitness}, elapsed time: #{SparqlChromosome.get_fastest_time}") }
-    }
+      File.open("result/#{timestr}/#{cnt}_fastest.txt", 'w') { |f| f.write("All times Fastest fit: Chr #{SparqlChromosome.fastest_chromosome} => #{SparqlChromosome.fastest_fitness}, elapsed time: #{SparqlChromosome.fastest_time}") }
+    end
 
     # return best solution
-    puts "All times Best fit: Chr #{alltime_best.value} => #{alltime_best.fitness_value}, elapsed time: #{alltime_best.get_elapsed_time}"
-    puts "All times Fastest fit: Chr #{SparqlChromosome.get_fastest_chromosome} => #{SparqlChromosome.get_fastest_fitness}, elapsed time: #{SparqlChromosome.get_fastest_time}"
+    puts "All times Best fit: Chr #{alltime_best.value} => #{alltime_best.fitness_value}, elapsed time: #{alltime_best.elapsed_time}"
+    puts "All times Fastest fit: Chr #{SparqlChromosome.fastest_chromosome} => #{SparqlChromosome.fastest_fitness}, elapsed time: #{SparqlChromosome.fastest_time}"
 
-    "#{alltime_best.value} => #{alltime_best.fitness_value}, elapsed time: #{alltime_best.get_elapsed_time}"
+    "#{alltime_best.value} => #{alltime_best.fitness_value}, elapsed time: #{alltime_best.elapsed_time}"
   end
 
   # Croossover method  is OX
   def crossover(selection, chromosome)
     i1 = rand(0..@@chr_size - 1)
     i2 = rand(i1..@@chr_size - 1)
-    if i1 == i2
-        return selection
-    end
+    
+    return selection if i1 == i2
+
     a1 = selection[0].value
     a2 = selection[1].value
-    cr1 = Array.new(@@chr_size,-1)
-    cr2 = Array.new(@@chr_size,-1)
+    cr1 = Array.new(@@chr_size, -1)
+    cr2 = Array.new(@@chr_size, -1)
     i1.upto(i2) do |i|
       cr1[i] = a1[i]
       cr2[i] = a2[i]
@@ -171,14 +171,14 @@ SPARQL
   @executed_sparql = ''
   # timestr
   @@timestr = ''
-  def self.get_timestr
+  def self.timestr
     @@timestr
   end
   def self.set_timestr
     # create result directory
     if @@timestr == ''
       t = Time.now
-      timestr = t.strftime("%Y%m%dT%H%M%S")
+      timestr = t.strftime('%Y%m%dT%H%M%S')
       @@timestr = timestr
       @@output_sparql_directgory = FileUtils.mkdir_p("result/#{timestr}/sparql")[0]
       @@output_time_directory = FileUtils.mkdir_p("result/#{timestr}/time/")[0]
@@ -212,16 +212,16 @@ SPARQL
     @resulttimearray = sga.exec_sparql_query(@executed_sparql, @@number_of_attemps)
     sortedsort = @resulttimearray.dup
     sortedsort.sort!
-    @resulttime = sortedsort[sortedsort.size/2]
-    #
+    @resulttime = sortedsort[sortedsort.size / 2]
+
     if @resulttime == -1
       @fitness_value = 0
     else
-      @fitness_value = 1/@resulttime
+      @fitness_value = 1 / @resulttime
     end
     # record all time best
     fastest_time = sortedsort[0]
-    fastest_fit = 1/fastest_time
+    fastest_fit = 1 / fastest_time
     if @@alltime_best_fitness_value < fastest_fit
       @@alltime_best_fitness_value = fastest_fit
       @@alltime_best_resulttime = fastest_time
@@ -230,16 +230,16 @@ SPARQL
     @@fitness_value_cache[@value] = @fitness_value
     @@elapsed_time_cache[@value] = @resulttime
     save_result
-    return @fitness_value
+    @fitness_value
   end
 
   def mutate(probability_of_mutation)
     value.each_with_index do |x, i|
-      if rand < probability_of_mutation
-        pos = rand(0..(SIZE-1))
-        @value[i] = @value[pos]
-        @value[pos] = x
-      end
+      next if rand > probability_of_mutation
+
+      pos = rand(0..(SIZE - 1))
+      @value[i] = @value[pos]
+      @value[pos] = x
     end
   end
 
@@ -248,30 +248,32 @@ SPARQL
     prefix = @value.join('_')
     # save result
 
-    File.open("#{@@output_sparql_directgory}/#{prefix}.rq", 'w') { |f|
+    File.open("#{@@output_sparql_directgory}/#{prefix}.rq", 'w') do |f|
       f.puts @executed_sparql
-    }
+    end
     # save elapsed time
-    File.open("#{@@output_time_directory}/#{prefix}.time.txt", 'w') { |f|
+    File.open("#{@@output_time_directory}/#{prefix}.time.txt", 'w') do |f|
       f.puts @resulttime
-    }
+    end
     # save elapsed time array
-    File.open("#{@@output_timearray_directory}/#{prefix}.timearray.txt", 'w') { |f|
-      f.puts @resulttimearray.join(",")
-    }
+    File.open("#{@@output_timearray_directory}/#{prefix}.timearray.txt", 'w') do |f|
+      f.puts @resulttimearray.join(',')
+    end
   end
 
-  def get_elapsed_time
+  def elapsed_time
     @@elapsed_time_cache[@value]
   end
 
-  def self.get_fastest_time
+  def self.fastest_time
     @@alltime_best_resulttime
   end
-  def self.get_fastest_fitness
+
+  def self.fastest_fitness
     @@alltime_best_fitness_value
   end
-  def self.get_fastest_chromosome
+
+  def self.fastest_chromosome
     @@alltime_best_value
   end
 end
@@ -279,7 +281,7 @@ end
 
 # ga = SPARQLGA.new(10)
 # # show ga @@char_size
-# puts ga.get_chr_size
+# puts ga.chr_size
 # #chr= ga.generate(SparqlChromosome)
 # # puts chr.chr
 # puts ga.run(SparqlChromosome, 0.2, 0.01, 100)
